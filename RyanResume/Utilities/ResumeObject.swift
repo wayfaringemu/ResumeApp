7c1e8ab53dd0159cc9d8bdb42285bf6ce5542d4d
wayfaringemu <rykowalspt@gmail.com>
2018-11-24 11:30:19 -0500

    added object class and some organization

---
+++ RyanResume/Utilities/ResumeObject.swift
@@ -0,0 +1,14 @@
+//
+//  ResumeObject.swift
+//  RyanResume
+//
+//  Created by ryan kowalski on 11/24/18.
+//  Copyright © 2018 ryan kowalski. All rights reserved.
+//
+
+import UIKit
+
+class ResumeObject: NSObject {
+    
+
+}
