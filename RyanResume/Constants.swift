70f7ecb676db84bf23b76347e9be6d850461bbaf
wayfaringemu <rykowalspt@gmail.com>
2018-11-24 11:28:15 -0500

    added Constants and Loading file, as well as AF

---
+++ RyanResume/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  RyanResume
+//
+//  Created by ryan kowalski on 11/24/18.
+//  Copyright © 2018 ryan kowalski. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let apiKey = "15429800845bf801f48e335"
+    static let apiUrl = "http://mockx.io/apis/15429800845bf801f48e335"
+}
---
+++ RyanResume/LoadingViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoadingViewController.swift
+//  RyanResume
+//
+//  Created by ryan kowalski on 11/24/18.
+//  Copyright © 2018 ryan kowalski. All rights reserved.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+
+    }
+    
+    func setupView() {
+        
+    }
+
+    
+    func getAPIData() {
+        
+    }
+    
+    func navigateToNextScreen() {
+        
+    }
+}
